5c598aaa90fce2a516741ccb7b79937b27c72850
Sara Nolan <saraa.ann28@gmail.com>
2021-04-22 14:33:25 -0400

    updated code for offline access

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,8 +1,11 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "app.js",
-    "favicon.ico"
+    "/index.js",
+    "/favicon.ico",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = "static-cache-v2";
